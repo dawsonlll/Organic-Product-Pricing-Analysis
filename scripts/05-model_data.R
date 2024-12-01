8968e4bded006c15657ad2f933c94f810f193a4c
Dawson Li <dawsonli@mail.utoronto.ca>
2024-11-30 19:34:46 -0500

    Update models
    fixed modelling

--- scripts/05-model_data.R
+++ scripts/05-model_data.R
@@ -16,7 +16,7 @@ library(rstanarm)
 analysis_data <- read_parquet("data/02-analysis_data/analysis_data.parquet")
 
 ### Model data ####
-first_model <- stan_glm(
+bayesian_price_model <- stan_glm(
   formula = current_price ~ month + old_price + vendor,  # Defines the regression model
   data = analysis_data,                                 # Specifies the dataset
   family = gaussian(),                                  # Assumes a normal distribution for the response variable
@@ -26,24 +26,10 @@ first_model <- stan_glm(
   seed = 304                                         # Ensures reproducibility
 )
 
-second_model <- stan_glm(
-  formula = current_price ~ month + current_price + vendor,  # Defines the regression model
-  data = analysis_data,                                 # Specifies the dataset
-  family = gaussian(),                                  # Assumes a normal distribution for the response variable
-  prior = normal(location = 0, scale = 2.5, autoscale = TRUE), # Sets normal priors for coefficients
-  prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE), # Sets normal prior for the intercept
-  prior_aux = exponential(rate = 1, autoscale = TRUE), # Sets exponential prior for auxiliary parameters (e.g., error SD)
-  seed = 304                                         # Ensures reproducibility
-)
 
 #### Save model ####
 saveRDS(
-  first_model,
-  file = "models/first_model.rds"
-)
-
-saveRDS(
-  second_model,
-  file = "models/second_model.rds"
+  bayesian_price_model,
+  file = "models/bayesian_price_model.rds"
 )
 
