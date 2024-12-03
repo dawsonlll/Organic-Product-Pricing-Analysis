67cc1bd1a01af6a78c1993f69ca52bc6a46801f9
Dawson Li <dawsonli@mail.utoronto.ca>
2024-12-03 03:51:00 -0500

    Fixed Preamble
    Updated preamble

--- scripts/05-model_data.R
+++ scripts/05-model_data.R
@@ -1,11 +1,11 @@
 #### Preamble ####
-# Purpose: Models... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Create Bayesian Model
+# Author: Dingshuo Li
+# Date: 01 December, 2024
+# Contact: dawson.li@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: analysis_data is saved as parquet in data files
+# Any other information needed? N/A
 
 
 #### Workspace setup ####
