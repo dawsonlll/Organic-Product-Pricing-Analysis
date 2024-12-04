f1b1dedc7be678bca6e9fb3728a3df724cb88e9b
Dawson Li <dawsonli@mail.utoronto.ca>
2024-12-03 20:52:03 -0500

    Update modelling
    Fixed typo in the organic-analysis-data

--- scripts/05-model_data.R
+++ scripts/05-model_data.R
@@ -13,12 +13,12 @@ library(tidyverse)
 library(rstanarm)
 
 #### Read data ####
-analysis_data <- read_parquet("data/02-analysis_data/analysis_data.parquet")
+organic_analysis_data <- read_parquet("data/02-analysis_data/analysis_data.parquet")
 
 ### Model data ####
 bayesian_price_model <- stan_glm(
   formula = current_price ~ month + old_price + vendor,  # Defines the regression model
-  data = analysis_data,                                 # Specifies the dataset
+  data = organic_analysis_data,                                 # Specifies the dataset
   family = gaussian(),                                  # Assumes a normal distribution for the response variable
   prior = normal(location = 0, scale = 2.5, autoscale = TRUE), # Sets normal priors for coefficients
   prior_intercept = normal(location = 0, scale = 2.5, autoscale = TRUE), # Sets normal prior for the intercept
